cb3bd1cba404ed230354465b1171181671869db2
KYEONGMIN CHO <ggingmin@gmail.com>
2024-08-05 13:45:49 +0900

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,6 @@ module.exports = {
   content: ["./src/main/resources/static/**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: '#3B82F6',
-        secondary: '#10B981',
-      }
     },
   },
   plugins: [
